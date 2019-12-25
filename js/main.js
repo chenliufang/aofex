193023f9e6492bad872a0e55b4d7d0922713ff08
chenliufang <67847466@qq.com>
2019-12-25 14:21:21 +0800

    修改HTML文件

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var data = [];
-var url = "https://bird.ioliu.cn/v2?url=https://option.aofex.com/webApi/scene/getResultList?timeId=1&pairId=1&pageNo=1&pageSize=40";
-var klineUrl150 = "https://bird.ioliu.cn/v2?url=https://option.aofex.com/webApi/scene/getKline?symbol=BTC/USDT&period=5min&size=200";
+var url = "https://bird.ioliu.cn/v2?url=https://option.aofex.com/webApi/scene/getResultList?timeId=1&pairId=1&pageNo=1&pageSize=30";
+var klineUrl150 = "https://bird.ioliu.cn/v2?url=https://option.aofex.com/webApi/scene/getKline?symbol=BTC/USDT&period=5min&size=100";
 var wssURL = "wss://option.aofex.com/websocket/optionKline/BTC-USDT/5min";
 
 function getLocalTime(nS) {
